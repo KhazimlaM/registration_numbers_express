72a810d807831ec2660520bbf635cd80ff519393
Khazimla <20113497@rcconnect.co.za>
2022-10-11 15:17:20 +0200

    database changes

--- database.sql
+++ database.sql
@@ -2,17 +2,17 @@ sudo -u postgres createdb registration;
 sudo -u postgres createuser pythagorus;
  
 
-create table town (
+create table towns (
 	id serial primary key,
-	Halmesbay text not null,
-    Bellville text not null,
-    CapeTown text not null
+	town text not null,
+    town_code text not null
 );
 
 create table registrationNumbers (
 	id serial not null primary key,
-    town_id integer not null,
     RegNumbers text not null,
-    foreign key (town_id) references town (id)
+    registration_code int not null,
+    foreign key (registration_code) references towns (id)
    
 );
+
