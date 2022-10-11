3ac70de14cc2a2ebbc70d7e012caab81b3891828
Khazimla <20113497@rcconnect.co.za>
2022-10-11 14:25:21 +0200

    UI changes

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE if not exists my_towns(
+id SERIAL PRIMARY KEY,
+my_towns text NOT NULL,
+counter int NOT NULL 
+);
+ALTER TABLE 
+ALTER COLUMN towns data_type NOT NULL;
+
+
+
+CREATE TABLE if not exists reg_numbers(
+id SERIAL PRIMARY KEY,
+reg_numbers text NOT NULL,
+counter int NOT NULL 
+);
